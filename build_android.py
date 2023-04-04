d8e78445df73d77de80ab0a13ccd9d0c9c4e2ae6
aLaix <alan.limagalvez@yahoo.com>
2023-04-04 17:53:07 -0600

    Completing build_android.py and build_wasm32.sh

--- build_android.py
+++ build_android.py
@@ -3,10 +3,12 @@
 """
 Make sure to change the configuration section according to your needs.
 
+Dependencies: java, android sdk, cmake, rust
+
 Many thanks to https://julhe.github.io/posts/building-an-android-app-with
 -rust-and-sdl2/
 """
-
+from contextlib import suppress
 from os import makedirs, mkdir, environ, pathsep, chdir, getcwd
 from os.path import exists, expanduser
 from shutil import copytree, rmtree, copy
@@ -31,15 +33,40 @@ def comment_line(file, line_to_comment):
             if line == line_to_comment:
                 line = f'# {line}'
                 found = True
-            f.writelines(line)
+            f.write(f'{line}\n')
     if not found:
         raise LookupError(
             f'Could not find line "{line_to_comment}" in file {file}')
 
 
 def process_section_in_gradle(file, section, comment_action):
-    # TODO
-    pass
+    comment_stem = '//'
+
+    with open(file, 'r') as f:
+        contents = f.read()
+
+    change = None
+    with open(file, 'w') as f:
+        for line in contents.splitlines():
+            stripped = line.strip()
+            if comment_action:
+                if stripped.startswith(section):
+                    change = True
+            else:
+                if stripped.startswith(comment_stem):
+                    if stripped[2:].strip().startswith(section):
+                        change = False
+            if change:
+                f.write(f'{comment_stem} {line}\n')
+            elif change is None:  # This case should come first otherwise not
+                # change will capture these both
+                f.write(f'{line}\n')
+            elif not change:
+                line = line.replace(comment_stem, '', 1)
+                f.write(f'{line}\n')
+
+            if stripped.startswith('}'):  # End of section
+                change = None
 
 
 def check_path_exists(path, name):
@@ -48,16 +75,20 @@ def check_path_exists(path, name):
             f'{name} not found at: {path}')
 
 
-def build_for_android():
+def build_android():
     original_cwd = getcwd()
     rust_sdl2_repo = f'../{RUST_SDL2_REPO}'
     check_path_exists(rust_sdl2_repo, 'Rust-SDL2 repo')
 
-    if exists(f'../{ANDROID_PROJECT_NAME}'):
+    if not exists(f'../{ANDROID_PROJECT_NAME}'):
         ndk_path = expanduser(NDK_PATH)
         check_path_exists(ndk_path, 'NDK path')
         environ['PATH'] += pathsep + ndk_path
-        copytree(f'../{RUST_SDL2_REPO}/sdl2-sys/SDL', '..')
+        chdir(f'../{rust_sdl2_repo}/sdl2-sys')
+        check_call('cargo build', shell=True)  # In fact, we only need this so
+        # it auto downloads the SDL source code
+        chdir(original_cwd)
+        copytree(f'../{RUST_SDL2_REPO}/sdl2-sys/SDL', '../SDL')
         chdir('../SDL')
         check_call('ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk '
                    'APP_PLATFORM=android-19', shell=True)
@@ -74,17 +105,18 @@ def build_for_android():
         chdir(original_cwd)
 
     # Copy sdl2 libs into rust's build dir
-    makedirs('target/aarch64-linux-android/$BUILD_MODE/deps/', exist_ok=True)
-    makedirs('target/armv7-linux-androideabi/$BUILD_MODE/deps/', exist_ok=True)
-    makedirs('target/i686-linux-android/$BUILD_MODE/deps/', exist_ok=True)
+    makedirs(f'target/aarch64-linux-android/{BUILD_MODE}/deps/', exist_ok=True)
+    makedirs(f'target/armv7-linux-androideabi/{BUILD_MODE}/deps/',
+             exist_ok=True)
+    makedirs(f'target/i686-linux-android/{BUILD_MODE}/deps/', exist_ok=True)
     copytree(f'{SDL2_LIBS_PATH}/arm64-v8a',
-             'target/aarch64-linux-android/$BUILD_MODE/deps/',
+             f'target/aarch64-linux-android/{BUILD_MODE}/deps/',
              dirs_exist_ok=True)
     copytree(f'{SDL2_LIBS_PATH}/armeabi-v7a',
-             'target/armv7-linux-androideabi/$BUILD_MODE/deps/',
+             f'target/armv7-linux-androideabi/{BUILD_MODE}/deps/',
              dirs_exist_ok=True)
     copytree(f'{SDL2_LIBS_PATH}/x86',
-             'target/i686-linux-android/$BUILD_MODE/deps/',
+             f'target/i686-linux-android/{BUILD_MODE}/deps/',
              dirs_exist_ok=True)
 
     # Build the libraries
@@ -98,7 +130,8 @@ def build_for_android():
 
     # Prepare folders...
     jni_libs_path = f'../{ANDROID_PROJECT_NAME}/app/src/main/jniLibs'
-    rmtree(f'{jni_libs_path}')
+    with suppress(FileNotFoundError):
+        rmtree(f'{jni_libs_path}')
     mkdir(f'{jni_libs_path}')
     mkdir(f'{jni_libs_path}/arm64-v8a')
     mkdir(f'{jni_libs_path}/armeabi-v7a')
@@ -118,8 +151,8 @@ def build_for_android():
     if exists(assets_path):
         rmtree(assets_path)
     mkdir(assets_path)
-    copytree('assets', assets_path)
+    copytree('assets', f'{assets_path}/assets')
 
 
 if __name__ == '__main__':
-    build_for_android()
+    build_android()
--- build_wasm32.sh
+++ build_wasm32.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/env bash
+
+# Many thanks to https://github.com/tung/ruggrogue/blob/master/book/dependencies.md
+
 if [ ! -d target/deploy ]; then
 mkdir -p target/deploy
 fi
 
 source "$HOME/emsdk/emsdk_env.sh" # Replace with your emscripten sdk's path
-# export EMCC_CFLAGS="-s USE_SDL=2 -o web/index.html"
 cargo build --target=wasm32-unknown-emscripten --release
 echo
 echo You can now open a browser at http://localhost:8000
