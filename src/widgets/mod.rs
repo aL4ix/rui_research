9d62d29591b494c9e8505a4e9313540c0b6a96c3
aL4ix <alan.limagalvez@yahoo.com>
2023-05-01 17:42:27 -0600

    Renaming to image

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -2,7 +2,7 @@ pub use button::Button;
 pub use primitives::Primitive;
 pub use text_box::TextBox;
 pub use widget::Widget;
-pub use img::Image;
+pub use image::Image;
 pub use common_widget::CommonWidget;
 
 mod primitives;
@@ -10,5 +10,5 @@ mod button;
 mod widget;
 mod events;
 mod text_box;
-mod img;
+mod image;
 mod common_widget;
