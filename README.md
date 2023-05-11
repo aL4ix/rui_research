6b026b33e87ce9732ff68c79279e76a76c68b3c9
aL4ix <alan.limagalvez@yahoo.com>
2023-05-11 17:51:15 -0600

    Adding more info to README.md and build_wasm32.sh

--- README.md
+++ README.md
@@ -1,11 +1,38 @@
-# Requisites
+# How to run it
 
-## Install SDL2 dev libraries
+## Linux
 
-Ubuntu:
+### Ubuntu:
+1. Install Rust
+   1. With rustup: https://www.rust-lang.org/tools/install
+   2. Or distro packages
+      > sudo apt install rust-all
+2. Install SDL2 dev libraries
+   >  sudo apt install libsdl2-dev
+3. Run it
+   > cargo run
 
->  sudo apt install libsdl2-dev
+### Alpine linux
+1. Install Rust
+   > sudo apk add cargo
+2. Install SDL2 dev libraries
+   > sudo apk add sdl2-dev
+3. Run it
+   > cargo run
 
-Windows:
+## Windows:
+1. Install Rust: https://www.rust-lang.org/tools/install 
+2. Install SDL2: https://wiki.libsdl.org/SDL2/Installation
+3. Run it
+   > cargo run
 
-https://wiki.libsdl.org/SDL2/Installation
+## Web:
+1. Install Rust: https://www.rust-lang.org/tools/install
+2. Install target
+   > rustup target add wasm32-unknown-emscripten
+3. Install emscripten: https://emscripten.org/docs/getting_started/downloads.html
+4. Open build_wasm32.sh and update EMSDK_PATH to point it to your emsdk installation's path
+5. Run it
+   > ./build_wasm32.sh
+6. Open a browser at http://localhost:8000
+ 
--- build_wasm32.sh
+++ build_wasm32.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
-
 # Many thanks to https://github.com/tung/ruggrogue/blob/master/book/dependencies.md
+# And https://web.archive.org/web/20210618192808/https://blog.therocode.net/2020/10/a-guide-to-rust-sdl2-emscripten
+
+# Update this to point it to your emsdk installation's path
+EMSDK_PATH=$HOME/emsdk
 
 if [ ! -d target/deploy ]; then
-mkdir -p target/deploy
+  mkdir -p target/deploy
 fi
 
-source "$HOME/emsdk/emsdk_env.sh" && # Replace with your emscripten sdk's path
-cargo build --bin rui_research --target=wasm32-unknown-emscripten --release &&
-echo &&
-echo &&
-echo =========================================================================== &&
-echo You can open a browser at http://localhost:8000. CTRL-C to close the server &&
-python3 -m http.server --directory target/deploy
+source "$EMSDK_PATH/emsdk_env.sh" &&
+  cargo build --bin rui_research --target=wasm32-unknown-emscripten --release &&
+  echo &&
+  echo &&
+  echo =========================================================================== &&
+  echo You can open a browser at http://localhost:8000. CTRL-C to close the server &&
+  python3 -m http.server --directory target/deploy
